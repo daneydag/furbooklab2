f12a2151c93696e9917f0174d739de5567e23414
daneydag <dgonzalez9413@richmondhillhs.org>
2021-12-01 16:46:15 +0000

    furbooklab2s

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import PetNav from './components/pet-nav'
-import PictureBox from './components/picture-box'
+import PetNav from './components/pet-nav';
+import PictureBox from './components/picture-box';
+import AboutBox from './components/about-box';
+import Posts from './components/posts';
 import './App.css';
 
 const App = () => {
-  const component = new React.Component()
-
-  component.render = () => {
-    return(
-      <div className="App">
-        <PetNav/>
-        <div class="container">
-          <div class="row">
-            //Add profile picture and about section here
-          </div>
-          <div class="row">
-            //Add posts here
+    const component = new React.Component()
+  
+    component.render = () => {
+      return(
+        <div className="App">
+          <PetNav/>
+          <div class="container">
+            <div class="row">
+              <PictureBox/>
+              <AboutBox/>
+            </div>
+            <div class="row">
+              <Posts/>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
+  
+    return component
   }
-
-  return component
-}
-
-export default App;
+  
+  export default App;
+  
